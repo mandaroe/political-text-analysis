964d5ad2f48c8916fb2e3ec95f0a2c2c31029d86
Amanda Rolle <amr2409@columbia.edu>
2026-04-01 19:44:45 -0400

    Update create_dataset.py

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -1,20 +1,19 @@
+
+
 import os
-from huggingface_hub import login
-
-token = os.getenv("HF_TOKEN")
-login(token=token)
-
 from datasets import load_dataset
-dataset = load_dataset("Eugleo/us-conghressional-speeches")
+from urllib.parse import urlparse
+import kagglehub
+import pandas as pd
+from google.colab import files
 
-sample_speech = dataset.shuffle(seed = 33).select(range(1000))
+path = kagglehub.dataset_download("gandpablo/news-articles-for-political-bias-classification")
 
-split1 = sample_speech.train_test_split(test_size = 0.3, seed = 33)
+news = pd.read_csv(
+    f"{path}/bias_clean.csv",
+    encoding="latin1",
+    on_bad_lines="skip",
+    low_memory=True,
+    nrows=100000
+)
 
-train = split1["train"]
-temp = split1["test"]
-
-split2 = temp.train_test_split(test_size = 0.5, seed = 33)
-
-val = split2["train"]
-test = split2["test"]
