51053b199476e8bfa77f8f20d08d23023a143bfb
Amanda Rolle <amr2409@columbia.edu>
2026-04-02 13:44:34 -0400

    Update create_dataset.py

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -1,4 +1,6 @@
-
+# ------ 
+# Imports
+# ------
 
 import os
 from datasets import load_dataset
@@ -6,6 +8,10 @@ from urllib.parse import urlparse
 import kagglehub
 import pandas as pd
 from google.colab import files
+
+# ------ 
+# Data
+# ------
 
 path = kagglehub.dataset_download("gandpablo/news-articles-for-political-bias-classification")
 
@@ -17,3 +23,16 @@ news = pd.read_csv(
     nrows=100000
 )
 
+# ------
+# Process
+# ------
+
+allowed_topics = [
+    "elections",
+    "economy-and-jobs",
+    "defense-and-security",
+    "immigration",
+    "donald-trump"
+]
+
+news = news[news['topic'].isin(allowed_topics)]
